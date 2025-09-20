465f15da5f72fb63a9a83208bae4d2cb5ec8599a
sypark <soyoung12055@gmail.com>
2025-09-21 02:12:23 +0900

    뉴스 검색 결과로 롤백

--- search/search_news.py
+++ search/search_news.py
@@ -17,18 +17,21 @@ ssl._create_default_https_context = ssl._create_unverified_context
 def get_search_context(keyword):
     if not keyword:
         return ""
-    response = get_news_search_response(keyword)
-    return "\n".join(delete_html_tag(document["title"]) for document in response["items"])
-
+    search_result = ""
+    for url in ["https://openapi.naver.com/v1/search/news.json"]:
+        response = get_naver_search_response(keyword, url)
+        search_result += "\n".join(delete_html_tag(document["title"]) + delete_html_tag(document["description"])
+                                   for document in response["items"])
+    return search_result
 
 def delete_html_tag(content):
     soup = BeautifulSoup(content, "lxml")
     return soup.get_text()
 
 
-def get_news_search_response(keyword):
+def get_naver_search_response(keyword, url):
     query = urllib.parse.quote(keyword)
-    url = f"https://openapi.naver.com/v1/search/webkr.json?query={query}&display=5&start=1&sort=sim"
+    url = f"{url}?query={query}&display=10&start=1&sort=sim"
     request = urllib.request.Request(url)
     request.add_header("X-Naver-Client-Id", client_id)
     request.add_header("X-Naver-Client-Secret", client_secret)
