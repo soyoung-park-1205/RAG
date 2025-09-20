5fab1d373ca14576a1554657d9441f493659acbf
sypark <soyoung12055@gmail.com>
2025-09-20 15:33:53 +0900

    evaluation 시에 질문에 등장한 nouns 제외

--- evaluator/evaluate.py
+++ evaluator/evaluate.py
@@ -1,9 +1,12 @@
 from preprocess.extract_keyword import get_nouns, get_nouns_okt
 
 
-def judge_answer_by_nouns(answer, context):
+def judge_answer_by_nouns(answer, context, question):
+    question_nouns = get_nouns_okt(question)
     answer_nouns = get_nouns_okt(answer)
     context_nouns = get_nouns_okt(context)
+    answer_nouns = [x for x in answer_nouns if x not in question_nouns]
+    context_nouns = [x for x in context_nouns if x not in question_nouns]
     count = len([noun for noun in answer_nouns if noun in context_nouns])
     if len(answer_nouns) == 0:
         return False
